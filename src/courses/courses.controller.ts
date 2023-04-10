388231b65f477bd73adc18a8217759198601c0f3
Luan Oliveira <luanoliveira17@hotmail.com>
2023-04-10 16:52:12 -0300

    feat: add find one course; add create course

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
   findAll() {
     return 'Courses list';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Course #${id}`;
+  }
+
+  @Post()
+  create(@Body('name') body) {
+    return body;
   }
 }
