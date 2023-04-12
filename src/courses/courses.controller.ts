6767272cfeb20e2dc92bf0443037e3dbd5bb6f96
Luan Oliveira <luanoliveira17@hotmail.com>
2023-04-12 17:15:03 -0300

    Injecting service into controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -3,12 +3,9 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
@@ -17,28 +14,27 @@ export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Get()
-  findAll(@Res() response) {
-    return response.status(200).send('Courses list');
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Course #${id}`;
+    return this.coursesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  create(@Body('name') body) {
-    return body;
+  create(@Body() body) {
+    return this.coursesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `Update of Course #${id}`;
+    return this.coursesService.update(id, body);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string, @Body() body) {
-    return `Delete of Course #${id}`;
+  remove(@Param('id') id: string) {
+    return this.coursesService.remove(id);
   }
 }
