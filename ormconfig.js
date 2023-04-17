88989b3d8028c55ffe0b71badc75abccc5bcdcba
Luan Oliveira <luanoliveira17@hotmail.com>
2023-04-17 16:17:12 -0300

    Creating docker-compose file

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: 5432,
   username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  password: 'docker',
+  database: 'nestjscourse',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   cli: {
