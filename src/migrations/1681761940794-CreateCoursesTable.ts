3355036e9c3a6742d53dc84c7d7bd741c63e3300
Luan Oliveira <luanoliveira17@hotmail.com>
2023-04-17 17:25:12 -0300

    Creating tables through migrations

---
+++ src/migrations/1681761940794-CreateCoursesTable.ts
@@ -0,0 +1,36 @@
+import { query } from 'express';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCoursesTable1681761940794 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'courses',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('courses');
+  }
+}
